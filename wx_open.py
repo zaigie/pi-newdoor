d7c1808c6edd9f0a260a23299f80f4a95c924ee4
JoyceWedamson <jokerwho@yeah.net>
2019-04-29 08:23:09 +0800

    修改DS3230舵机占空比

--- wx_open.py
+++ wx_open.py
@@ -2,38 +2,45 @@ import itchat
 import time
 import RPi.GPIO as GPIO
 '''set GPIO'''
-GPIO.setmode(BCM)
+GPIO.setmode(BOARD)
 GPIO.setwarnings(False)
-open = 17
+open = 12
+fPWM = 50
+a = 10
+b = 2
+
+def setup():
+    global pwm
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(open, GPIO.OUT)
+    pwm = GPIO.PWM(open, fPWM)
+    pwm.start(0)
 
 '''set itchat to login wechat and search group'''
 itchat.auto_login(hotReload=True)
 rooms = itchat.get_chatrooms(update=True)
-rooms = itchat.search_chatrooms(name='itchat_test')
+rooms = itchat.search_chatrooms(name='open')
 if rooms is not None:
     username = rooms[0]['UserName']
 else:
     username = 'filehelper'
 
 '''opendoor'''
-def OpenDoor(angle):
-	assert angle >=30 and angle <= 150
-	pwm = GPIO.PWM(open, 50)
-	pwm.start(8)
-	dutyCycle = angle / 18. + 3.
-	pwm.ChangeDutyCycle(dutyCycle)
-	time.sleep(0.3)
-	pwm.stop()
+def OpenDoor(direction):
+    duty = a / 180 * direction + b
+    pwm.ChangeDutyCycle(duty)
+    print("direction =", direction, "-> duty =", duty)
+    time.sleep(1)
 
 '''Listen wechat msg'''
 @itchat.msg_register(itchat.content.TEXT,isGroupChat=True)
 def reply_msg(msg):
     if msg['Content'] == u'开门':
-            GPIO.setup(open, GPIO.OUT)
-            OpenDoor(90)
-            time.sleep(1.5)
-            OpenDoor(30)
-            itchat.send_msg("欢迎回家！",toUserName=username)
+            setup()
+            OpenDoor(45)
+            time.sleep(1)
+            OpenDoor(0)
+            itchat.send_msg("欢迎回来！" ,toUserName=username)
             GPIO.cleanup()
 
 if __name__ == '__main__':
